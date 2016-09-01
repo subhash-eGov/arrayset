cf2d7ca6cc33a1516016789e56ba6c5d6adb687c
subhash.bose@egovernments.org <subhash.bose@egovernments.org>
2016-09-01 14:57:49 +0530

    Code refactoring

--- src/main/java/org/arrayset/ArraySet.java
+++ src/main/java/org/arrayset/ArraySet.java
@@ -1,49 +1,35 @@
 package org.arrayset;
 
+import java.util.Arrays;
 
 public class ArraySet {
-    private Integer[] es;
+    private Integer[] elements;
 
     public ArraySet() {
-        this.es = new Integer[0];
+        this.elements = new Integer[0];
     }
 
     public Integer[] toArray() {
-        return this.es;
+        return this.elements;
     }
 
     public void addNewElemnt(Integer element) {
-        int i = 0;
-        int e = 0;
-        if (this.es.length == 0) {
-            if (element == null) {
-                throw new NullPointerException();
-            }
-            this.es = new Integer[1];
-            this.es[0] = element;
-            return;
-        }
-        for (i = 0; i < this.es.length; i++) {
-            if (this.es[i].equals(element)) {
-                e = 1;
-            } else {
-                continue;
-            }
-        }
-        if (e == 0) {
-            Integer[] anotherElements = new Integer[this.es.length + 1];
-            for (i = 0; i < this.es.length; i++) {
-                anotherElements[i] = this.es[i];
-            }
+        if (!exists(element)) {
+            elements = Arrays.copyOf(elements, elements.length + 1);
             if (element == null) {
                 throw new NullPointerException();
             } else {
-                anotherElements[this.es.length] = element;
-                this.es = anotherElements;
+                elements[this.elements.length - 1] = element;
             }
-
-        } else {
-
         }
+    }
+
+    private boolean exists(Integer element) {
+        for (int i = 0; i < this.elements.length; i++) {
+            if (this.elements[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
